df460fdb95af731a9f64c718273940b70c8d13d5
GeneralPoxter <GeneralPoxter@users.noreply.github.com>
2020-01-18 12:51:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
 // Server setup
 // Code from https://devcenter.heroku.com/articles/node-websockets
 const express = require('express');
-const {
-    Server
-} = require('ws');
+const {Server} = require('ws');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
 const server = express()
-    .use(express.static("public", {
-        root: '.'
-    }))
+    .use(express.static("public", {root: '.'}))
     .get("/", function(req, res) {
-        res.sendFile("index.html", {
-            root: 'public'
-        })
+        res.sendFile("index.html", {root: 'public'})
     })
     .listen(PORT, () => console.log("start"));
 
-const wss = new Server({
-    server
-});
+const wss = new Server({server});
 const connection = require("./src/WSSManager.js");
 
 wss.on('connection', connection);
